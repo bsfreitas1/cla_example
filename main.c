3fbfdb8aa122f5a29e81fbb762e854ac9cd99ea2
Bernardo Silveira <bernardo.silveira@estudante.ufjf.br>
2026-06-21 22:58:02 -0300

    Buck com controle implementado no CLA funcionando

--- main.c
+++ main.c
@@ -1,42 +1,59 @@
-
-//
-// Included Files
-//
 #include "driverlib.h"
 #include "device.h"
 #include "board.h"
-#include "math.h"
+#include "scicomm.h"
+#include "shared_vars.h"
 
-#pragma DATA_SECTION(fVal,"CpuToCla1MsgRAM");
-float fVal=0;
-#pragma DATA_SECTION(fResult,"Cla1ToCpuMsgRAM");
-float fResult=0;
+// Mapeamento explícito nas Message RAMs de acordo com o Linker
+#pragma DATA_SECTION(vo, "CpuToCla1MsgRAM");
+float vo = 0.0f;
 
+#pragma DATA_SECTION(duty_cycle_cla, "Cla1ToCpuMsgRAM");
+float duty_cycle_cla = 0.0f;
+
+
+
+// Flag de sincronização entre a CPU e o CLA
+volatile uint16_t cla_task_complete = 0;
 
 void main(void)
 {
-        Device_init();
+    // Inicialização do sistema e dos periféricos via SysConfig
+    Device_init();
+    Interrupt_initModule();
+    Interrupt_initVectorTable();
+    Board_init();
 
-        Interrupt_initModule();
+    EINT;
+    ERTM;
 
-        Interrupt_initVectorTable();
-
-        Board_init();
-        EINT;
-        ERTM;
-
-        for(;;)
+    while (1)
+    {
+        // Verifica se o pacote de 4 bytes (float) do PLECS chegou à FIFO da SCI
+        if (SCI_getRxFIFOStatus(SCI0_BASE) > 3)
         {
-            CLA_forceTasks(myCLA0_BASE,CLA_TASKFLAG_1);
-            DEVICE_DELAY_US(100000);
-        }
+            // Recebe a tensão de saída do conversor
+            protocolReceiveData(SCI0_BASE, &vo, sizeof(float));
 
+            // Sinaliza o início e força a execução da Task 1 no CLA
+            cla_task_complete = 0;
+            CLA_forceTasks(myCLA0_BASE, CLA_TASKFLAG_1);
+
+            // Aguarda o término do cálculo pelo CLA
+            while (cla_task_complete == 0)
+            {
+                // Loop de espera ativo até a ISR ser chamada
+            }
+
+            // Envia a razão cíclica calculada de volta para o simulador
+            protocolSendData(SCI0_BASE, &duty_cycle_cla, sizeof(float));
+        }
+    }
 }
 
-__interrupt void cla1Isr1 ()
+// Interrupção executada quando o CLA finaliza a Task 1
+__interrupt void cla1Isr1(void)
 {
-    fVal +=0.1f;
-    if(fVal>2*M_PI) fVal=0;
+    cla_task_complete = 1;
     Interrupt_clearACKGroup(INT_myCLA01_INTERRUPT_ACK_GROUP);
 }
-
--- shared_vars.h
+++ shared_vars.h
@@ -1,14 +1,10 @@
-/*
- * shared_vars.h
- *
- *  Created on: 4 de jul de 2025
- *      Author: Prof. Guilherme Marcio Soares
- */
-
 #ifndef SHARED_VARS_H_
 #define SHARED_VARS_H_
 
-extern float fVal;
-extern float fResult;
+// Variável atualizada pela CPU com o dado vindo do PLECS
+extern float vo;
+
+// Variável atualizada pelo CLA com o resultado do controlador
+extern float duty_cycle_cla;
 
 #endif /* SHARED_VARS_H_ */
